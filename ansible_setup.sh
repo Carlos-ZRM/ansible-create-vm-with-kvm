656a7691cdd6bd88705ce08aaa90db79114f547b
Carlos-ZRM <carloszrm90@gmail.com>
2023-01-18 21:47:59 -0600

    Create KVM cluster

--- ansible_setup.sh
+++ ansible_setup.sh
@@ -3,11 +3,11 @@
 
 ROOT_DIR="./ansible"
 ANSIBLE_DIRS="env,handlers,meta,tasks,templates,files,vars"
-ANSIBLE_ENVS="common,local"
-ANSIBLE_GROUPS="group1,group2"
-TASKS="task1,task2"
+ANSIBLE_ENVS="local"
+ANSIBLE_GROUPS="kvm_hosts_local"
+TASKS="install_packages,cloud_images,cloud_init,create_vm"
 
-rm -r $ROOT_DIR
+#rm -r $ROOT_DIR
 mkdir $ROOT_DIR
 
 # Create main structure
@@ -15,7 +15,7 @@ bash -c "mkdir -p  ${ROOT_DIR}/{${ANSIBLE_DIRS}}"
 bash -c "touch ${ROOT_DIR}/{${ANSIBLE_DIRS}}/main.yml"
 
 # create env directories
-bash -c "mkdir -p ${ROOT_DIR}/env/{${ANSIBLE_ENVS}}/group_vars/{${ANSIBLE_GROUPS}}/"
+bash -c "mkdir -p ${ROOT_DIR}/env/{${ANSIBLE_ENVS}}/group_vars/{${ANSIBLE_GROUPS}}"
 bash -c "touch ${ROOT_DIR}/env/{${ANSIBLE_ENVS}}/hostname.yml"
 bash -c "touch ${ROOT_DIR}/env/{${ANSIBLE_ENVS}}/group_vars/{${ANSIBLE_GROUPS}}/{vars.yml,vault.yml}"
 
